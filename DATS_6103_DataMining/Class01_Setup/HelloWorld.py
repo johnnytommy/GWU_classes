8e542e9e10a58c30aa4797a961d341c6ee6be305
Edwin Lo <edwinlo@gwu.edu>
2020-01-17 10:49:46 -0500

    new Jan 17

--- DATS_6103_DataMining/Class01_Setup/HelloWorld.py
+++ DATS_6103_DataMining/Class01_Setup/HelloWorld.py
@@ -1,6 +1,12 @@
-print("Hello world!")
+#%%
+# print("Hello world!")
 print(5 / 8)
 print (7+10)
 
+#%%
 astr = "Thank you again and again and again!"
+anumber = 3.14159265358979323846
 print("Surprise!! %s " % astr )
+print("I love sweetie %f" % anumber )
+print("I love sweetie %d" % anumber )
+print("I love sweetie %.3f" % anumber )
