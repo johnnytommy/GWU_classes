cea4ecfda28a1334d6af745394355a93251d2c02
Edwin Lo <edwinlo@gwu.edu>
2020-01-23 13:49:34 -0500

    week 2 init

--- DATS_6103_DataMining/Class01_Setup/HelloWorld.py
+++ DATS_6103_DataMining/Class01_Setup/HelloWorld.py
@@ -4,9 +4,13 @@ print(5 / 8)
 print (7+10)
 
 #%%
-astr = "Thank you again and again and again!"
-anumber = 3.14159265358979323846
-print("Surprise!! %s " % astr )
-print("I love sweetie %f" % anumber )
-print("I love sweetie %d" % anumber )
-print("I love sweetie %.3f" % anumber )
+astring = "Thank you"
+anum = 3.14159265358979323846
+print("I want to say %s" % astring )
+print("I want to say " + astring )
+print("I want to say ", astring )
+print("I want to say %s, my sweetie %.3f" % (astring,anum) )
+print("I want to say %s, my sweetie digit %d" % (astring,anum) )
+print("I want to say %s, my sweetie long %f" % (astring,anum) )
+# see https://python-reference.readthedocs.io/en/latest/docs/str/formatting.html
+# s-string, d-digit (int), f-float
