c7dec6aab6ae577e0fc16b8e98ad25f970d51e28
Johnny Tommy <johnthomasthegreat@gmail.com>
2020-02-06 15:34:18 -0500

    Hw completr
    HW 3 Compelted

--- DATS_6103_DataMining/Class03_classes/Week03_hw.py
+++ DATS_6103_DataMining/Class03_classes/Week03_hw.py
@@ -6,36 +6,36 @@
 ###################################### Q1 ###############################
 # let us write a function find_grade(total) 
 # which will take your course total (0-100), and output the letter grade (see your syllabus)
-# have a habbit of putting in the docstring
+# have a habit of putting in the docstring
 total = 62.1
 
 def find_grade(total):
-  # write an appropriate and helpful docstring #TODO
+  # Function that takes an integer course total (total) and returns the letter grade.
   if   total >= 93:
     grade = "A"
     return grade
-  elif total >=90 and total <=92:
+  elif total >=90 and total <93:
     grade = "A-"
     return grade
-  elif total >=87 and total <=89:
+  elif total >=87 and total <90:
     grade = "B+"
     return grade
-  elif total >=83 and total <=86:
+  elif total >=83 and total <87:
     grade = "B"
     return grade
-  elif total >=80 and total <=82:
+  elif total >=80 and total <83:
     grade = "B-"
     return grade
-  elif total >=77 and total <=79:
+  elif total >=77 and total <80:
     grade = "C+"
     return grade
-  elif total >=73 and total <=76:
+  elif total >=73 and total <77:
     grade = "C"
     return grade
-  elif total >=70 and total <=72:
+  elif total >=70 and total <73:
     grade = "C-"
     return grade    
-  elif total >=60 and total <=69:
+  elif total >=60 and total <70:
     grade = "D"
     return grade
   else:
@@ -48,10 +48,10 @@ print(find_grade(total))
 
 # Also answer these: 
 # What is the input (function argument) data type for total? 
-##INTEGER
+"INTEGER"
 
 # What is the output (function return) data type for find_grade(total) ?
-##STRING
+"STRING"
 
 #%%
 ###################################### Q2 ###############################
@@ -60,7 +60,7 @@ print(find_grade(total))
 grade = 'C-'
 
 def to_gradepoint(grade):
-  # write an appropriate and helpful docstring #TODO
+# Function to take a string letter grade (grade) and return the grade point
   if   grade == "A":
     gradepoint = 4.0
     return gradepoint
@@ -97,10 +97,10 @@ def to_gradepoint(grade):
 print(to_gradepoint(grade))
 
 # What is the input (function argument) data type for find_grade? 
-##STRING
+"STRING"
 
 # What is the output (function return) data type for find_grade(grade) ?
-##INTEGER
+"INTEGER"
 
 #%%
 ###################################### Q3 ###############################
@@ -109,7 +109,7 @@ print(to_gradepoint(grade))
 course = { "class":"IntroDS", "id":"DATS 6101", "semester":"spring", "year":2018, "grade":'B-', "credits":3 } 
 
 def to_gradepoint_credit(course):
-  # write an appropriate and helpful docstring #TODO
+  # Function that takes a dict of course information (course) and returns the total weight grade points of the singular course
   grade_point_credit = to_gradepoint(course['grade']) * course['credits']
   return grade_point_credit
 
@@ -117,10 +117,10 @@ def to_gradepoint_credit(course):
 print(" %.2f " % to_gradepoint_credit(course) )
 
 # What is the input (function argument) data type for to_gradepoint_credit? 
-##DICT
+"DICT"
 
 # What is the output (function return) data type for to_gradepoint_credit(course) ?
-##INT
+"INT"
 
 #%%
 ###################################### Q4 ###############################
@@ -137,18 +137,23 @@ courses = [
   ]
 
 def find_gpa(courses):
-  # write an appropriate and helpful docstring #TODO
+  # Take list of all courses and return total GPA
   total_grade_point_credit =0 # initialize 
   total_credits =0 # initialize
-  # ??????    fill in your codes here
-  # gpa = ?????
+  for i in courses:
+    total_grade_point_credit += to_gradepoint_credit(i)
+    total_credits += i['credits']
+    gpa = total_grade_point_credit /total_credits
   return gpa
 
 # Try:
 print(" %.2f " % find_gpa(courses) )
 
 # What is the input (function argument) data type for find_gpa? 
-# What is the output (function return) data type for find_gpa(courses) ?
+"LIST"
+
+# What is the output (function return) data type for find_gpa(courses)?
+"INTEGER"
 
 
 #%%
@@ -159,17 +164,25 @@ print(" %.2f " % find_gpa(courses) )
 course = { "class":"IntroDS", "id":"DATS 6101", "semester":"spring", "year":2018, "grade":'B-', "credits":3 } 
 
 def printCourseRecord(course):
-  # write an appropriate and helpful docstring
-  # use a single print() statement to print out a line of info as shown here
-  # 2018 spring - DATS 6101 : Intro to DS (3 credits) B-  Grade point credits: 8.10 
-  # ??????    fill in your codes here
-  return # or return None
+  # Function that takes a course (DICT) prints a grade record for the course (STRING)
+  year  = course['year']
+  sem   = course['semester']
+  id1    = course['id']
+  class1 = course['class']
+  cred  = course['credits']
+  grade = course['grade']
+  gradepoint = to_gradepoint_credit(course)
+  
+  print("%d %s - %s : %s (%d credits) %s Grade point credits: %.2f" % (year,sem,id1,class1,cred,grade, gradepoint) )
   
 # Try:
 printCourseRecord(course)
 
 # What is the input (function argument) data type for printCourseRecord? 
+"DICT"
+
 # What is the output (function return) data type for printCourseRecord(course) ?
+"STRING"
 
 
 #%%
@@ -181,20 +194,19 @@ printCourseRecord(course)
 # Cumulative GPA: ?????
  
 def printTranscript(courses):
-  # write an appropriate and helpful docstring
+  # Function that takes a list of courses (LIST) and returns a print statement of the complete transcript with GPA
   for course in courses:
-    # print out each record as before
-  
-  # after the completion of the loop, print out a new line with the gpa info
-  
-  return # or return None
+   printCourseRecord(course)
+  gpa = find_gpa(courses)
+  print("Cumulative GPA: %.2f" % gpa)
 
-# Try to run, see if it works as expected to produce the desired result
-# courses is already definted in Q4
 printTranscript(courses)
 
 # What is the input (function argument) data type for printTranscript? 
+"LIST"
+
 # What is the output (function return) data type for printTranscript(courses) ?
+"STRING"
 
 
 
@@ -215,10 +227,10 @@ def fib(n):
   :param n: the index, starting from 0
   :return: the sequence
   """
-  # assume n is positive integer
-  # ??????    fill in your codes here
-
-  return # return what ????
+  if n <= 1:
+    return n
+  else:
+    return(fib(n-1) + fib(n-2))
 
 
 # Try:
